0dbf3efaf3c2dc5f5212ca7c66d3c9a5fbdacdd6
shira <shirag0101@gmail.com>
2026-04-12 15:22:09 +0300

    תיקון itemp.temp2.aspx

---
+++ shiragoldsteinwebsite/item_temp2.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public string st = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+            string itemName = Request.Form["itemName"];
+            string itemDescription = Request.Form["itemDescription"];
+            string itemPrice = Request.Form["itemPrice"];***
+            string meat = Request.Form["meat"];
+
+
+
+
+            string sql =
+                "INSERT INTO tItems (itemName, itemDescription, itemPrice ) " +
+                "VALUES(" +
+                "N'" + itemName +
+                "', N'" + itemDescription +
+                "', N'" + itemPrice +
+                "', N'" + meat +
+                "')";
+
+            MyAdoHelper.DoQuery("App_Data\\myDB.mdf", sql);
+
+            st = "DONE";
+        }
+    }
+}
